78600a39a8975c79b922b2ae50a06e4091bfb1ab
Adrian Kuendig <adriankue@gmail.com>
2012-08-20 16:41:42 +0200

    removed unnecessary files

--- src/test/resources/ok.js
+++
@@ -1,5 +0,0 @@
-var a = 5;
-var f = function(x) {
-    return 2 * x;
-};
-//@ sourceMappingURL=ok.js.map
--- src/test/resources/ok_import.js
+++
@@ -1,6 +0,0 @@
-imported = require("./imported");
-util = require("./util");
-var f = function(x) {
-    return a(x);
-};
-//@ sourceMappingURL=ok_import.js.map
